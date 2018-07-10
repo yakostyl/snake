b691e476d60a3baca534f251275a60ea66fdfdb4
yakostyl <a.kostylev@mail.ru>
2018-07-10 14:08:19 +0300

    исправил ошибку

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,13 +10,16 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1;
-            int y1;
-            char sym1;
-            x1 = 5;
-            y1 = 7;
+            int x1 = 5;
+            int y1 = 7;
+            char sym1 = '*';
             sym1 = '*';
             Draw(x1, y1, sym1);
+
+            int x2 = 7;
+            int y2 = 66;
+            char sym2 = '#';
+            Draw(x2, y2, sym2);
 
             Console.ReadLine();
         }
