0a871652660fb664d4a5005f1eb100fe295fccb5
yakostyl <a.kostylev@mail.ru>
2018-07-10 14:55:50 +0300

    Make Class point? witch draw a symbol in coordinates.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,24 +10,20 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 5;
-            int y1 = 7;
-            char sym1 = '*';
-            sym1 = '*';
-            Draw(x1, y1, sym1);
+        Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
 
-            int x2 = 7;
-            int y2 = 66;
-            char sym2 = '#';
-            Draw(x2, y2, sym2);
+        
 
+        Point p2 = new Point();
+            p2.x = 5;
+            p2.y = 7;
+            p2.sym = '#';
+            p2.Draw();
             Console.ReadLine();
-        }
-
-        private static void Draw (int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
+        }                    
     }
 }
